4702cd3dd086cff196174518de757efd290ee8ad
Bratanas <nojue.ciesna@gmail.com>
2025-02-18 12:17:18 +0200

    Add Tailwind CSS configuration and components for UI layout

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,53 @@
-import { Stack, Link } from 'expo-router';
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { Text } from "@/components/ui/text";
+import { Box } from "@/components/ui/box";
+import { HStack } from "@/components/ui/hstack";
+import { VStack } from "@/components/ui/vstack";
+import { Pressable } from "@/components/ui/pressable";
+import { Icon } from "@/components/ui/icon";
+import { Menu } from "lucide-react-native";
 
-import { Button } from '~/components/Button';
-import { Container } from '~/components/Container';
-import { ScreenContent } from '~/components/ScreenContent';
-
-export default function Home() {
+const HomeScreen = () => {
   return (
-    <>
-      <Stack.Screen options={{ title: 'Home' }} />
-      <Container>
-        <ScreenContent path="app/index.tsx" title="Home" />
-        <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
-          <Button title="Show Details" />
-        </Link>
-      </Container>
-    </>
+    <Box className="flex-1 justify-center align-center p-4" >
+      <Box className="absolute top-0 left-0 right-0 p-4" >
+        <HStack space="lg" reversed={true} >
+          <Pressable>
+            <Icon as={Menu} size={'xl'} color="black" />
+          </Pressable>
+        </HStack>
+      </Box>
+
+      <Box className=" justify-center align-center p-4 " >
+
+        {/* Logo */}
+        <Text className=" text-center mb-4 " size={"3xl"} >
+          JUNG
+        </Text>
+
+        {/* Card with Buttons */}
+        <Box className="justify-center p-4 bg-[#C2C2C2] rounded-2xl shadow-md ">
+          <Text className="color-black text-center mb-2" size={"lg"}>
+            Išmanu. Saug. Lankstu
+          </Text>
+          
+          <VStack className="justify-center" space="md">
+            <Button className="bg-white rounded-2xl" variant="outline" size="lg">
+              <Text className="color-black" size={"2xl"}>Sistemų informacija</Text>
+            </Button>
+            <Button className="bg-white rounded-2xl" variant="outline" size="lg">
+              <Text className="color-black" size={"2xl"}>Atraskite sprendimą</Text>
+            </Button>
+            <Button className="bg-white rounded-2xl" variant="outline" size="lg">
+              <Text className="color-black" size={"2xl"}>Informacija</Text>
+            </Button>
+          </VStack>
+        </Box>
+
+      </Box>
+    </Box>
   );
-}
+};
+
+export default HomeScreen;
